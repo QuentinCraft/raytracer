c63b076e4b952a59d0f40ea0a219b59dacafc54f
epi-noahg <noah.gosciniak@epitech.eu>
2023-05-11 13:01:57 +0200

    Fix: cone

--- src/objects/Cone.cpp
+++ src/objects/Cone.cpp
@@ -27,7 +27,7 @@ namespace RayTracer {
     std::optional<PipeLine> Cone::hits(Ray const& ray) const {
         Math::Vector3D rayOrigin = ray._origin - _origin;
         PipeLine pipe;
-        pipe._info = "Cone";
+        pipe._info = "ConeFront";
 
         double r2 = _radius * _radius;
         double h2 = _height * _height;
@@ -42,24 +42,10 @@ namespace RayTracer {
             return std::nullopt;
 
         double t1 = (-b - sqrt(discriminant)) / (2 * a);
-        double t2 = (-b + sqrt(discriminant)) / (2 * a);
-
-        double y1 = ray._origin._y + t1 * ray._direction._y;
-        double y2 = ray._origin._y + t2 * ray._direction._y;
-
-        if ((y1 < _origin._y || y1 > _origin._y + _height) && (y2 < _origin._y || y2 > _origin._y + _height))
-            return std::nullopt;
 
         double t = t1;
-        if (y1 < _origin._y || y1 > _origin._y + _height) {
-            t = t2;
-        } else if (y2 >= _origin._y && y2 <= _origin._y + _height && y2 < y1) {
-            t = t2;
-        }
 
         Math::Vector3D hitPoint = ray._origin + ray._direction * t;
-        if (hitPoint._y < _origin._y || hitPoint._y > _origin._y + _height)
-            return std::nullopt;
 
         double t_top = (_origin._y + _height - ray._origin._y) / ray._direction._y;
 
@@ -68,9 +54,12 @@ namespace RayTracer {
         double dist_top = sqrt((hitPoint_top._x - _origin._x) * (hitPoint_top._x - _origin._x) + (hitPoint_top._z - _origin._z) * (hitPoint_top._z - _origin._z));
 
         if (Math::Utils::inf(dist_top, _radius) || Math::Utils::equal(dist_top, _radius)) {
-            pipe._position = hitPoint_top;
-            pipe._info = "TopCone";
-        }
+            if (Math::Utils::inf(Math::Utils::distance(ray._origin, hitPoint_top), Math::Utils::distance(ray._origin, hitPoint))) {
+                pipe._position = hitPoint_top;
+                pipe._info = "TopCone";
+            }
+        } else if (Math::Utils::inf(hitPoint._y, _origin._y) || Math::Utils::sup(hitPoint._y, _origin._y + _height))
+            return std::nullopt;
         std::pair<std::shared_ptr<IMaterial>, Math::Vector3D> mat = _texture->getTexture(pipe._position);
         pipe._object = std::make_shared<Cone>(*this);
         pipe._material = mat.first;
@@ -85,6 +74,8 @@ namespace RayTracer {
             return Math::Vector3D(0, -1, 0);
         else if (pipe._info == "Cone")
             return Math::Vector3D((_origin - pipe._position)._x, 0, (_origin - pipe._position)._z * -1);
+        else if (pipe._info == "ConeFront")
+            return Math::Vector3D((_origin - pipe._position)._x, 0, (_origin - pipe._position)._z);
         return Math::Vector3D();
     }
 
